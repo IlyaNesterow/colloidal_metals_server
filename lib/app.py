e9262cc71fedf7249becaf4f1adb98701a64895c
IlyaNesterow <j.nesterov1709@gmail.com>
2021-03-04 19:27:28 +0200

    added cors headers for credentials

--- lib/app.py
+++ lib/app.py
@@ -16,7 +16,7 @@ def after_request(response: Response) -> Response:
     header = response.headers
     header['Access-Control-Allow-Origin'] = 'http://localhost:3000'
     header['Access-Control-Allow-Methods'] = 'PUT, GET, DELETE, OPTIONS'
-    header['Access-Control-Allow-Headers'] = '*'
+    header['Access-Control-Allow-Headers'] = 'content-type, authorization'
     header['Access-Control-Allow-Credentials'] = 'true'
     return response
 
