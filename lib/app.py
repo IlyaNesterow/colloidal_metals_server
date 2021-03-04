abee262dc5cecab273343cfa75b05e63443ff091
IlyaNesterow <j.nesterov1709@gmail.com>
2021-03-04 19:21:55 +0200

    added cors for credentials

--- lib/app.py
+++ lib/app.py
@@ -14,9 +14,9 @@ load_dotenv(find_dotenv())
 @app.after_request
 def after_request(response: Response) -> Response:
     header = response.headers
-    header['Access-Control-Allow-Origin'] = '*'
+    header['Access-Control-Allow-Origin'] = 'http://localhost:3000'
     header['Access-Control-Allow-Methods'] = '*'
-    header['Access-Control-Allow-Headers'] = 'Content-Type, Authorization'
+    header['Access-Control-Allow-Headers'] = '*'
     header['Access-Control-Allow-Credentials'] = 'true'
     return response
 
