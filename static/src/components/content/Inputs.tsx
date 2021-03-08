fa145e834dfb9198c462b0a13dc60e312eb77a1f
IlyaNesterow <j.nesterov1709@gmail.com>
2021-03-08 23:04:17 +0200

    added inputs

---
+++ static/src/components/content/Inputs.tsx
@@ -0,0 +1,98 @@
+import React from 'react'
+
+interface Props<T> {
+  label: string 
+  value: T
+  onChange: React.FormEventHandler<HTMLInputElement | HTMLTextAreaElement>
+}
+
+interface NumericInput extends Props<number> {
+  min?: number
+  max?: number
+}
+
+export const NumberInput: React.FC<NumericInput> = ({ min = 0, max = 0, label, ...props }) => {
+  return(
+    <div>
+      <p>{ label }</p>
+      <input 
+        type="number" 
+        max={ max }
+        min={ min }
+        { ...props }
+      />
+    </div>
+  )
+}
+
+interface UrlProps extends Props<string> {
+  placeholder?: string
+}
+
+export const UrlInput: React.FC<UrlProps> = ({ label, placeholder = 'url', ...props }) => {
+  return(
+    <div>
+      <p>{ label }</p>
+      <input 
+        type="number" 
+        placeholder={ placeholder }
+        { ...props }
+      />
+    </div>
+  )
+}
+
+interface TextAreaProps extends Props<string> {
+  placeholder: string
+  min: number
+  max: number
+}
+
+export const TextInput: React.FC<TextAreaProps> = ({ min, max, label, ...props }) => {
+  return(
+    <div>
+      <p>{ label }</p>
+      <textarea
+        maxLength={ max }
+        minLength={ min }
+        { ...props }
+      />
+    </div>
+  )
+}
+
+interface TitleProps extends Props<string> {
+  placeholder?: string
+}
+
+export const TitleInput: React.FC<TitleProps> = ({ label, placeholder = '', ...props }) => {
+  return(
+    <div>
+      <p>{ label }</p>
+      <input 
+        type="text"
+        placeholder={ placeholder }
+        { ...props }
+      />
+    </div>
+  )
+}
+
+interface CheckBoxProps {
+  checked: boolean
+  label: string
+  onChange: React.FormEventHandler<HTMLInputElement>
+}
+
+export const Checkbox: React.FC<CheckBoxProps> = ({ label, ...props }) => {
+  return(
+    <div>
+      <label htmlFor="input-checkbox">{ label }</label>
+      <input
+        id="input-checkbox"
+        type="checkbox"
+        { ...props }
+      />
+    </div>
+  )
+}
